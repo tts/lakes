74aa99d35bb23503690ff3afe8db64bc0747b636
Tuija <tuija.sonkkila@aalto.fi>
2022-01-12 17:35:40 +0200

    Polygons and points from geofi

--- hkiarea.R
+++ hkiarea.R
@@ -8,73 +8,78 @@ wfs_request <- paste0(baseurl, "&typeName=", type, "&outputFormat=json")
 res <- st_read(wfs_request, quiet = TRUE, stringsAsFactors = FALSE)
 res_4326 <- st_transform(res, crs = 4326)
 
-# Lake area
 lakes_big_enough <- res_4326 %>% 
   mutate(area = as.numeric(st_area(.))) %>% # m2
   filter(area >= 10000) # 100 x 100 m
 
+get_names <- function(df) {
+  res <- as.data.frame(df["vesisto_nimi_s"]) %>% 
+    mutate(
+      lon = map_dbl(geometry,  ~st_centroid(.x)[[1]]),
+      lat = map_dbl(geometry,  ~st_centroid(.x)[[2]])
+    )
+}
+
+# Closests lake(s) to home
 home <- data.frame(latitude = 60.188081, longitude = 24.998965)
-home_sf <- st_as_sf(home, coords = c("longitude", "latitude"), 
-                  crs = 4326)
+home_sf <- st_as_sf(home, coords = c("longitude", "latitude"), crs = 4326)
 
-names <- as.data.frame(lakes_big_enough[3]) %>% 
-  mutate(
-    lon = map_dbl(geometry,  ~st_centroid(.x)[[1]]),
-    lat = map_dbl(geometry,  ~st_centroid(.x)[[2]])
-  ) 
+lakes_big_enough_dist <- lakes_big_enough %>% 
+  rowwise() %>% 
+  mutate(d_from_home = st_distance(home_sf, geometry)) %>% 
+  arrange(d_from_home)
 
-names_sample <- names %>% 
-  sample_n(50)
+names_closest <- get_names(lakes_big_enough_dist[1:5,])
 
-distance_to_lake <- st_distance(home_sf, lakes_big_enough)
-distance_to_lake_sorted <- sort(distance_to_lake)
+# https://ropengov.github.io/geofi/articles/geofi_making_maps.html
+polygon <- geofi::get_municipalities(year = 2021, scale = 4500)
+point <- geofi::municipality_central_localities
+point$municipality_code <- as.integer(point$kuntatunnus)
 
-most_near <- lakes_big_enough[which.min(st_distance(home_sf, lakes_big_enough[1:nrow(lakes_big_enough),])),]
+polygon_helsinki_area <- polygon %>% 
+  filter(maakunta_name_fi %in% "Uusimaa" & 
+           !nimi %in% c("Karkkila", "Siuntio", "Raasepori", "Hanko", "Inkoo", "Lohja", "Loviisa", 
+                        "Porvoo", "Myrskylä", "Lapinjärvi", "Pukkila", "Askola"))
 
-# TO DO Second most near?         
-
-name_most_near <- as.data.frame(most_near[3]) %>% 
-  mutate(
-    lon = map_dbl(geometry,  ~st_centroid(.x)[[1]]),
-    lat = map_dbl(geometry,  ~st_centroid(.x)[[2]])
-  ) 
-
-names_sample_and_nearest <- rbind(names_sample, name_most_near)
+point_helsinki_area <- point %>% 
+  filter(municipality_code %in% polygon_helsinki_area$municipality_code)
 
 ggplot(data = lakes_big_enough) +
+  geom_sf(data = polygon_helsinki_area, alpha = .3) + 
   geom_sf(fill = "#3498db") +
-  geom_label_repel(data = names_sample_and_nearest, 
+  geom_sf_text(data = point_helsinki_area, aes(label = teksti), size = 2) +
+  geom_label_repel(data = names_closest, 
                    aes(x = lon, y = lat, label = vesisto_nimi_s),
                    box.padding = unit(0.8, "lines"),
+                   segment.color = "orange",
                    max.overlaps = Inf,
-                   size = 2,
+                   size = 3,
                    nudge_x = 0.02,
                    nudge_y = 0.02,
                    xlim = c(-Inf, Inf), ylim = c(-Inf, Inf)) +
-  geom_point(data = names_sample[names_sample$vesisto_nimi_s != "",], 
-             x = names_sample$lon,
-             y = names_sample$lat, 
-             color = "blue") +
-  # geom_sf(data = home_sf, color = "orange") +
-  geom_sf(data = most_near, color = "green", size = 2) +
+  geom_point(data = names_closest[names_closest$vesisto_nimi_s != "",], 
+             x = names_closest$lon,
+             y = names_closest$lat, 
+             color = "orange") +
   theme_minimal() +
   coord_sf(clip = 'off') +
-  theme(plot.title = element_text(size = 12),
+  theme(plot.title = element_text(size = 18),
         legend.position = "none",
+        plot.caption = element_text(vjust = 4, size = 8),
         axis.title = element_blank(),
         axis.text = element_blank(),
         panel.grid.minor = element_blank(),
         panel.grid.major = element_line(colour = "transparent")) +
     labs(x = NULL, y = NULL, 
-         title = "Lakes around here big enough for skiing (100 x 100 m)",
-         caption = "Data: Helsinki region map\nhttps://hri.fi/data/dataset/seutukartta")
+         title = "Closests lakes from home big enough for cross-country skiing (100x100 m)",
+         caption = "Data: Helsinki region map https://hri.fi/data/dataset/seutukartta ; Statistics Finland via geofi | @ttso")
 
 
 ggsave(
   "skiinglakes.png",
   width = 35, 
   height = 25, 
-  dpi = 72, 
+  dpi = 320, 
   units = "cm", 
   device = 'png'
 )
