ee06965c6a5a262d9d6c3c16d0dcecd6e84f870d
Tuija <tuija.sonkkila@aalto.fi>
2022-01-23 15:42:50 +0200

    Initial commit

---
+++ app.R
@@ -0,0 +1,125 @@
+library(shiny)
+library(tidyverse)
+library(leaflet)
+library(sf)
+
+my_files <- list.files(pattern = "\\.RDS$")
+lakes <- list()
+lakes <- lapply(my_files, readRDS)
+
+sizes <- c("-10.000", "10.000-100.000", "100.000-1.000.000", "1.000.000-")
+
+ui <- fluidPage(
+  
+  tags$h2(
+    HTML("In the Helsinki area, find your closest lake(s) for cross-country skiing, skating etc.")
+  ),
+  
+  tags$head(
+    tags$style(HTML("
+      body {
+        background-color: #333333;
+        color: white;
+      },
+      .shiny-input-container {
+        color: snow;
+      }
+      label.control-label {
+        color: #5f9ea0;
+      }
+      .leaflet-container {
+       cursor: pointer !important;
+      }"
+    ))
+  ),
+  
+  sidebarPanel(
+    selectInput(inputId = "size",
+                label = "Size (m2)",
+                choices = sizes),
+    selectInput(inputId = "nr",
+                label = "Number",
+                choices = seq(1:5)),
+    HTML("<p></p>
+          <span style='color:black;font-size:12px'
+          <p>
+            Choose the lake size category, the number of lakes to show - and click the map.
+          </p>
+          <p></p>
+          <p>
+              -10.000 m2 <= 100x100 m<br/>
+              10.000-100.000 <= 300x300<br/>
+              100.000-1.000.000 <= 1000x1000
+          </p>
+          </span>"),
+    width = 3
+  ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Map", 
+               leafletOutput("map", height = 500, width = "100%"))
+    ),
+    width = 9
+  )
+)
+
+server <- function(input, output, session) {
+  
+  size_chosen <- reactive({
+    
+    if (input$size == "-10.000") {
+      lakes_from_here <- lakes[[1]] 
+    }
+    if (input$size == "10.000-100.000") {
+      lakes_from_here <- lakes[[2]] 
+    }
+    if (input$size == "100.000-1.000.000") {
+      lakes_from_here <- lakes[[3]] 
+    }
+    if (input$size == "1.000.000-") {
+      lakes_from_here <- lakes[[4]]
+    }
+    
+    lakes_from_here
+    
+  })
+  
+  output$map <- renderLeaflet({
+    
+    leaflet() %>%
+      addProviderTiles("CartoDB.Positron",
+                       options = providerTileOptions(minZoom = 9, maxZoom = 15)) %>% 
+      # Center for Martinlaakso
+      setView(lng = 24.869128, lat = 60.277815, zoom = 9)
+  })
+  
+  
+  observe({
+    
+    click = input$map_click
+    
+    if(is.null(click))
+      return()
+    
+    here <- data.frame(latitude = click$lat, longitude = click$lng)
+    here_sf <- st_as_sf(here, coords = c("longitude", "latitude"), crs = 4326)
+    
+    lake_category <- input$size
+    
+    lakes_from_here <- size_chosen() %>%
+      rowwise() %>%
+      mutate(d_from_here = st_distance(here_sf, geometry)) %>%
+      arrange(d_from_here)
+    
+    map_proxy = leafletProxy("map") %>%
+      clearShapes() %>%
+      addPolygons(data = lakes_from_here[1:input$nr, ]) %>% 
+      setView(lng = 24.869128, lat = 60.277815, zoom = 9)
+    
+  })
+  
+  
+}
+
+shinyApp(ui, server)
---
+++ prepare.R
@@ -0,0 +1,31 @@
+library(sf)
+library(tidyverse)
+
+baseurl <- "https://kartta.hel.fi/ws/geoserver/avoindata/wfs?request=GetFeature&service=WFS&version=2.0.0"
+type <- "avoindata:Seutukartta_maankaytto_jarvet"
+wfs_request <- paste0(baseurl, "&typeName=", type, "&outputFormat=json")
+res <- st_read(wfs_request, quiet = TRUE, stringsAsFactors = FALSE)
+res_4326 <- st_transform(res, crs = 4326)
+
+lakes <- res_4326 %>% 
+  mutate(area = as.numeric(st_area(.)),
+         size = cut(area, breaks = c(1, 10000, 100000, 1000000, 100000000), 
+                    labels = c("1-10.000", "10.000-100.000", "100.000-1.000.000", "1.000.000-"),
+                    include.lowest = TRUE))
+
+lakes1 <- lakes %>% 
+  filter(size == "1-10.000",
+         str_squish(vesisto_nimi_s) != "Lippajärvi", 
+         str_squish(vesisto_nimi_s) != "Pitkäjärvi")
+lakes2 <- lakes %>% 
+  filter(size == "10.000-100.000")
+lakes3 <- lakes %>% 
+  filter(size == "100.000-1.000.000")
+lakes4 <- lakes %>% 
+  filter(size == "1.000.000-")
+
+write_rds(lakes1, "lakes1.RDS")
+write_rds(lakes2, "lakes2.RDS")
+write_rds(lakes3, "lakes3.RDS")
+write_rds(lakes4, "lakes4.RDS")
+
